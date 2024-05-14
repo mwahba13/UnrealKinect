3b223abb5512b3d270b13cbbaea89d8a0828ef24
Michael Wahba <mrswahba13@gmail.com>
2024-05-14 13:21:53 -0700

    first

---
+++ Source/UnrealKinect/UnrealKinect.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealKinect.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealKinect, "UnrealKinect" );
